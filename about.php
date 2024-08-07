2d1cdae6d092c86ff7c9483a5f58d41eb6a02c35
alishbasoomro17 <soomro4500793@cloud.neduet.edu.pk>
2024-08-07 00:51:14 -0700

    Initial commit

---
+++ about.php
@@ -0,0 +1,106 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/23.png" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Developer's Message:</h3>
+         <p>Hey There ! I'm Alishba Fatima. A Student of BE in Software Engineering Department from NED University [Batch : 2022] . I love designing websites and exploring new things. Learning new things is my hobby.</p>
+
+    
+         <a href="contact.php" class="btn">Contact Us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+
+ 
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="#" ><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#" ><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="#" ><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="#" ><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Alishba Fatima </span> | all rights reserved!</div>
+
+</footer>
